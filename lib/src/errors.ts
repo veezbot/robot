0b9d94bf8c261daa5664bb23debf4a16c41287fc
Alexandre Wurtz <me@lxtz.fr>
2026-03-27 17:21:13 +0100

    Add ServiceUnavailable error code to ErrorCode enum

--- lib/src/errors.ts
+++ lib/src/errors.ts
@@ -1,6 +1,7 @@
 export const ErrorCode = {
-  MissingToken: 'MISSING_TOKEN',
-  InvalidToken: 'INVALID_TOKEN',
+  MissingToken:       'MISSING_TOKEN',
+  InvalidToken:       'INVALID_TOKEN',
+  ServiceUnavailable: 'SERVICE_UNAVAILABLE',
 } as const;
 
 export type ErrorCode = typeof ErrorCode[keyof typeof ErrorCode];
